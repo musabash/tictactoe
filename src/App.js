dbfcceb9dafa2ded375fbb6e07c006ac4de052e2
Musa Bas <basmusa@icloud.com>
2021-11-28 23:28:20 +0000

    index.css edited

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 import Box from './components/box' 
 
 function App() {
-  return (
-    <div className="container">
-      <h1>Tic Tac Toe</h1>
-      <div className="board">
-          <Box />
-          <Box />
-          <Box />
-          <Box />
-          <Box />
-          <Box />
-          <Box />
-          <Box />
-          <Box />
-      </div>
-    </div>
-  );
+    return (
+        <div className="container">
+            <h1>Tic Tac Toe</h1>
+            <h3 className="status">Status</h3>
+            <div className="board">
+                <Box />
+                <Box />
+                <Box />
+                <Box />
+                <Box />
+                <Box />
+                <Box />
+                <Box />
+                <Box />
+            </div>
+            <button>Reset</button>
+        </div>
+    );
 }
 
 export default App;
